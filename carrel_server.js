56ec23eadadf8dacf385887ac2540d44b73c1e1b
deepakverma9685 <deepakverma9685@gmail.com>
2019-02-10 16:36:56 +0530

    html

--- carrel_server.js
+++ carrel_server.js
@@ -8,6 +8,9 @@ app.use(expressValidator());
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(bodyParser.json());
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.use(express.static(path.join(__dirname, '/public')));
 var constant = require('./connection/constant');
 var user = require('./controller/user'); 
@@ -19,3 +22,24 @@ app.set('port', port);
 
 app.listen(port);
 user.configure(app);
+var thankyouindex = require('./controller/thankyou');
+var addbook = require('./controller/addbooks');
+
+app.use('/addbooks',addbook);
+app.use('/thank',thankyouindex);
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
---
+++ controller/addbooks.js
@@ -0,0 +1,71 @@
+var express = require('express');
+var router = express.Router();
+var dbconnect = require('../connection/db');
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    res.render('addbooks');
+});
+
+
+router.post('/post', function (req, res, nex) {
+
+    console.log(req.body.school)
+
+    var today = new Date();
+    var add_books = {
+        "user_id": 1,
+        "book_name":req.body.book_name,
+        "publisher": req.body.publisher,
+        "binding": req.body.binding,
+        "description":req.body.description,
+        "authour":req.body.authour,
+        "price":req.body.price,
+        "language":req.body.language,
+        "isbn":req.body.isbn,
+        "weigth":"1.5",
+        "school":req.body.school,
+        "city":req.body.city,
+        "state":3,
+        "country":1,
+        "classes":req.body.classes,
+        "publish":"yes",
+        "created_by":1,
+        "updated_by":1,
+        "status":"yes",
+        "created_on": today,
+        "updated_on": today
+    };
+
+    console.log(add_books);
+
+    if (add_books != null) {
+
+        dbconnect.query('INSERT INTO cr_products SET ?', add_books, function (error, results) {
+            if (error) {
+                console.log(error);
+                /* res.json({
+                     status:true,
+                     message:'there are some error with query'
+                 })*/
+            } else {
+                console.log(results);
+                var get_last_id = results.insertId;
+                var results = "";
+                dbconnect.query('SELECT * FROM cr_products WHERE id = ?', [get_last_id], function (error, results) {
+                    if (error) {
+                        console.log(error)
+                    } else {
+                        console.log(results);
+                        res.redirect('/thank')
+                    }
+                });
+
+            }
+        })
+    }
+
+
+});
+
+module.exports = router;
